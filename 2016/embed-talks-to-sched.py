5fec8714752bbecdb5e431c30b4937a7fddb5351
Takahiro Hirofuchi <t.hirofuchi@aist.go.jp>
2016-06-16 14:02:10 +0900

    add schedule scripts

---
+++ 2016/embed-talks-to-sched.py
@@ -0,0 +1,33 @@
+
+import sys
+import re
+
+rex = re.compile('((?:ARC\+CPSY\+DC|HPC|PRO|OS|MEPA)-\d+)')
+
+data = {}
+session_content = ""
+
+with open("os.txt") as f:
+    for line in f:
+        mo = rex.match(line)
+        if mo:
+            session_name = mo.group(1)
+            data[session_name] = line
+        else:
+            data[session_name] += line
+
+
+for session_name in data:
+    text = data[session_name]
+    text = text.strip()
+    text = text.replace('\n', '<br />')
+    data[session_name] = text
+
+
+with open("schedule-details.md", 'w') as fout:
+    with open("schedule-overview.md") as fin:
+        text = fin.read()
+        for session_name in data:
+            text = text.replace(session_name, data[session_name])
+
+        print >> fout, text
